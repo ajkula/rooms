ba33e4d820b722dcc11574557c578dc93535f2fe
Greg JEFTIC <ajkula@gmail.com>
2019-05-29 22:47:14 +0200

    fine tunning Decision.js

--- OLD_Decision.js
+++ OLD_Decision.js
@@ -1,11 +1,11 @@
 class Decision {
   constructor(enemy, target) {
-    console.log("constructor enemy:", enemy)
-    console.log("constructor target:", target)
+    console.log("constructor enemy:", enemy.name, ' HP: ', enemy.health)
+    console.log("constructor target:", target.name, ' HP: ', enemy.health)
     this.enemy = enemy;
     this.target = target;
-    console.log("constructor this.enemy:", this.enemy)
-    console.log("constructor this.target:", this.target)
+    console.log("constructor this.enemy:", this.enemy.name, ' HP: ', this.enemy.health)
+    console.log("constructor this.target:", this.target.name, ' HP: ', this.enemy.health)
     // this.fullHealth = enemy.fullHealth;
     // this.health = enemy.health;
     // this.strength = enemy.strength;
@@ -41,8 +41,8 @@ class Decision {
             potions: 0,
             actions: [
               {
-                label: "spell",
-                spell: 1,
+                label: "scroll",
+                scroll: 1,
                 actions: [
                   {
                     label: "leaf",
@@ -51,8 +51,8 @@ class Decision {
                 ]
               },
               {
-                label: "spell",
-                spell: 0,
+                label: "scroll",
+                scroll: 0,
                 actions: [
                   {
                     label: "leaf",
@@ -67,12 +67,11 @@ class Decision {
     ];
   }
 
-  rooting(tree) {
-    // if (tree[0].label === "leaf") { return enemy[tree.actions]() }
-    // console.log("tree", tree)
-    if (tree[0].label === "leaf") { 
+  rooting(tree = this.tree) {
+console.log("TREE", tree)
+    if (tree[0].label === "leaf") {
       console.log("*******************************"); 
-      console.log("actions: ", tree[0].actions); 
+      console.log("LEAF ACTIONS: ", tree[0].actions); 
       return this.enemy[tree[0].actions](this.target); 
     }
 
@@ -80,85 +79,20 @@ class Decision {
       if (this.between(tree[0].health, this.percent(this.enemy.fullHealth, this.enemy.health))) { this.rooting(tree[0].actions) }
       if (this.between(tree[1].health, this.percent(this.enemy.fullHealth, this.enemy.health))) { this.rooting(tree[1].actions) }
     }
+
     if (tree[0].label !== "leaf" && tree[0].label  !== "root") {
-      if (this.enemy[tree[0].label] >= tree[0][tree[0].label]) { this.rooting(tree[0].actions) }
-      else { this.rooting(tree[1].actions) }
+      console.log("CONDITION:", "(", tree[0].label, ")", this.enemy[tree[0].label], " >= ", tree[0][tree[0].label]);
+      if (this.enemy[tree[0].label] >= tree[0][tree[0].label]) { console.log("TREE 0:", tree[0].actions); this.rooting(tree[0].actions) }
+      else { console.log("TREE 1:", tree[1].actions); this.rooting(tree[1].actions) }
     }
   }
 
-  percent(base, actual) { 
-    return (actual / base * 100) % 1 === 0 ? actual / base * 100 : Math.floor(actual / base * 100);
-  }
-
-  between(array, n) {
-    return array[0] <= n && n <= array[1];
-  }
+  percent(base, actual) { return (actual / base * 100) % 1 === 0 ? actual / base * 100 : Math.floor(actual / base * 100); }
+  between(array, n) { return array[0] <= n && n <= array[1]; }
 }
 
 // ******************************* tests ********************************
-
-
-const tree = [
-  {
-    label: "root",
-    health: [50, 100],
-    actions: [
-      {
-        label: "leaf",
-        actions: "attack"
-      }
-    ]
-  },
-  {
-    label: "root",
-    health: [0, 49],
-    actions: [
-      {
-        label: "potions",
-        potions: 1,
-        actions: [
-          {
-            label: "leaf",
-            actions: "heal"
-          }
-        ]
-      },
-      {
-        label: "potions",
-        potions: 0,
-        actions: [
-          {
-            label: "skill",
-            skill: 1,
-            actions: [
-              {
-                label: "leaf",
-                actions: "useSkill"
-              }
-            ]
-          },
-          {
-            label: "skill",
-            skill: 0,
-            actions: [
-              {
-                label: "leaf",
-                actions: "attack"
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-];
-
-function between(array, n) {
-  return array[0] <= n && n <= array[1];
-}
-
-const Monster = require('./Characters').Enemy
-
+const Monster = require('./Characters').Enemy;
 const enemy = new Monster({ 
   name: "SKELETON", 
   fullHealth: 40,
@@ -166,38 +100,26 @@ const enemy = new Monster({
   strength: 15,
   potions: 0,
   avoid: 20,
-  spell: 1,
+  scroll: 2,
   dammage: 5,
-  skill: 1,
 });
 
-console.log(percent(enemy.fullHealth, enemy.health) + '%');
-
-function percent(base, actual) { 
-  console.log(actual, base, enemy.fullHealth, enemy.health)
-  return (actual / base * 100) % 1 === 0 ? actual / base * 100 : Math.floor(actual / base * 100);
+function percent(base, actual) {
+  return (actual / base * 100) % 1 === 0 ?
+   actual / base * 100 :
+   Math.floor(actual / base * 100);
 }
-
-function rooting(enemy, tree) {
-  // if (tree[0].label === "leaf") { return enemy[tree.actions]() }
-  console.log()
-  console.log("tree", tree)
-  if (tree[0].label === "leaf") { console.log(tree[0].actions); return 0 }
-  if (tree[0].label  === "root") {
-    if (between(tree[0].health, percent(enemy.fullHealth, enemy.health))) { rooting(enemy, tree[0].actions) }
-    if (between(tree[1].health, percent(enemy.fullHealth, enemy.health))) { rooting(enemy, tree[1].actions) }
-  }
-  if (tree[0].label !== "leaf" && tree[0].label  !== "root") {
-    if (enemy[tree[0].label] >= tree[0][tree[0].label]) { rooting(enemy, tree[0].actions) }
-    else { rooting(enemy, tree[1].actions) }
-  }
-}
+console.log("enemy health", percent(enemy.fullHealth, enemy.health) + '%');
 
 // console.clear()
 const a = new Decision(enemy, enemy)
-a.rooting(a.tree)
-console.log(percent(3600, 120) + '%');
-console.log(percent(enemy.fullHealth, enemy.health) + '%');
-rooting(enemy, tree)
+console.log();
+a.rooting()
+console.log();
+a.rooting()
+console.log();
+a.rooting()
+console.log();
+a.rooting()
 
 module.exports = Decision;
